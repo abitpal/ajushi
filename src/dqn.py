7ae027a51d9b1ee62209732364f55282c4b4d46f
pauldbd <loldeadmemegod@gmail.com>
2025-10-19 02:23:04 -0500

    stuff

--- src/dqn.py
+++ src/dqn.py
@@ -10,14 +10,19 @@ from stable_baselines3.common.atari_wrappers import WarpFrame
 # Download the model from the Hub
 model_path = hf_hub_download(repo_id="kuds/car-racing-dqn", filename="best_model.zip")
 
-# Load the model
-model = DQN.load(model_path)
-
 # Create the environment
+seed = 1
 env_kwargs_dict={"continuous": False}
-env = make_vec_env("CarRacing-v3", n_envs=1, env_kwargs=env_kwargs_dict, wrapper_class=WarpFrame)
+env = make_vec_env("CarRacing-v3", n_envs=1, env_kwargs=env_kwargs_dict, wrapper_class=WarpFrame, seed=1)
 env = VecFrameStack(env, n_stack=4)
 env = VecTransposeImage(env)
+
+# Load the model
+model = DQN.load(model_path, env=env, verbose=1)
+
+model.learn(total_timesteps=2048 * 5, progress_bar=True)
+
+model.save("dpo_post_trained")
 
 # Enjoy the trained agent
 obs = env.reset()
